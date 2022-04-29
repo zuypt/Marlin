f9323a31e7bbf0319aec4e8209d82579b2408d7c
unknown <phanthanhduypr@gmail.com>
2022-04-29 13:06:44 +0800

    update config for ender3pro - enable HOST_ACTION commands - enable center point bed tramming

--- configurations/Creality/Ender-3 Pro/CrealityV422/0/Configuration.h
+++ configurations/Creality/Ender-3 Pro/CrealityV422/0/Configuration.h
@@ -1694,7 +1694,7 @@
   #define LEVEL_CORNERS_INSET_LFRB { 30, 30, 30, 30 } // (mm) Left, Front, Right, Back insets
   #define LEVEL_CORNERS_HEIGHT      0.0   // (mm) Z height of nozzle at leveling points
   #define LEVEL_CORNERS_Z_HOP       4.0   // (mm) Z height of nozzle between leveling points
-  //#define LEVEL_CENTER_TOO              // Move to the center after the last corner
+  #define LEVEL_CENTER_TOO              // Move to the center after the last corner
   //#define LEVEL_CORNERS_USE_PROBE
   #if ENABLED(LEVEL_CORNERS_USE_PROBE)
     #define LEVEL_CORNERS_PROBE_TOLERANCE 0.1
